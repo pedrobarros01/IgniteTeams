911b88a58e55a066033042f1f9a5f8fb4c994cb2
Pedro Barros <pedrobarros0101@outlook.com>
2022-12-19 14:11:47 -0300

    Adicionando tema à aplicação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
+import { ThemeProvider } from 'styled-components';
 import { Groups } from '@screens/groups';
-
+import theme from './src/theme';
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 
