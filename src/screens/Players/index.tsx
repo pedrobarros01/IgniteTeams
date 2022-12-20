480a564ed23c9573b8ad10c23e4ee3657a3c7e7a
Pedro Barros <pedrobarros0101@outlook.com>
2022-12-20 14:47:34 -0300

    Adicionando componente PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,6 +6,9 @@ import { HighLight } from '@components/HighLight';
 import { Input } from '@components/Input';
 import * as S from './styles';
 import { useState } from 'react';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players(){
     const [team, setTeam] = useState('Time A');
@@ -47,6 +50,31 @@ export function Players(){
                 </S.NumberOfPlayers>
             </S.HeaderList>
             
+            <FlatList 
+                data={players}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <PlayerCard 
+                    name={item}
+                    onRemove={() => { }}
+                    />
+                )}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message="Não ha pessoas nesse time"
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    {paddingBottom: 100}, 
+                    players.length === 0 && { flex: 1}
+                ]}
+            />
+
+            <Button 
+                title="Remover Turma"
+                type="SECONDARY"
+            />
         </S.Container>
     );
 }
